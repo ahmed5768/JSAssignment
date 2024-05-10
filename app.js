3639d215f46224223accf423de1d295f4d7ebceb
ahmed5768 <101082079+ahmed5768@users.noreply.github.com>
2024-05-11 03:36:40 +0500

    done

---
+++ app.js
@@ -0,0 +1,73 @@
+// Task no 01
+// var date = new Date()
+// document.write(date)
+
+// Task no 02
+// const now = new Date();
+// const year = now.getFullYear();
+// const month = now.getMonth() + 1; // Months are zero-indexed, so January is 0, February is 1, etc.
+// const date = now.getDate();
+
+// console.log(`Today is ${date}/${month}/${year}`);
+
+// Task no 03
+// var weekName = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+// for(i = 2024 ; i <= 2050 ; i++){
+//     var day = new Date(i, 0, 1)
+//     if(day.getDay() == 0){
+//         console.log(weekName[day.getDay()],i);
+//     }
+// }
+
+// Task no 04
+// var number = 7
+// var random = +prompt("Enter a random integers", "1 to 10")
+// if(number === random){
+//     alert("Good Work")
+// }else{
+//     alert("Not matched")
+// }
+
+//Task no 05
+// let augustDate = new Date('2024-08-14')
+// let currentDate = new Date()
+// let dateDiff = augustDate - currentDate
+// console.log(dateDiff)
+// console.log(dateDiff / (1000 * 60 * 60 * 24))
+
+// Task no 06
+// var str = "Hello World";
+// str = str.slice(0, 3) + str.slice(4);
+// console.log(str)
+
+//Task no 07
+// var number = +prompt("Enter a number")
+// if(number % 3 === 0 || number % 7 === 0){
+//     alert("True")
+// }else{
+//     alert("Flase")
+// }
+
+// Task no 08
+// var start = prompt("Enter a word start java", "javascript")
+// if(start.startsWith("java")){
+//     alert(true);
+// }else{
+//     alert(false)
+// }
+
+// Task no 09
+// var twoIntegers = prompt("Enter a two integers")
+// var matched = false
+// for(i = 50 ; i <= 99 ; i++){
+//     if(twoIntegers == i){
+//         matched = true
+//         alert(true)
+//         break
+//     }
+// }
+// if(matched === false){
+//     alert(false)
+// }
+
+// Task no 10
